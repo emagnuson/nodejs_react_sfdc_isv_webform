ae78f5e112490778dddb9926e1f7efd240af188c
Eric Magnuson <eric.s.magnuson@gmail.com>
2020-12-15 15:36:22 -0800

    Update Trial.js

--- react-ui/src/containers/Trial.js
+++ react-ui/src/containers/Trial.js
@@ -1,10 +1,11 @@
 export class Trial {
-  constructor(firstName, lastName, email, company, phone, uname, countryCode, prefValue, phoneValue, langCode) {
+  constructor(firstName, lastName, email, company, phone, subdomain, uname, countryCode, prefValue, phoneValue, langCode) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.company = company;
     this.phone = phone;
+    this.subdomain = subdomain;
     this.uname = uname;
     this.countryCode = countryCode;
     this.prefValue = prefValue;
